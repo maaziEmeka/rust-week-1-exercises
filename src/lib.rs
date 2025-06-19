58f42e2d627d9eb57543f639ef8ad6dab6b0a67d
Maazi Emeka <maazigithub@gmail.com>
2025-06-19 18:36:17 -0400

    chore: remove unnecessary pattern logic.

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,4 @@
 // Implement extract_tx_version function below
-use hex::FromHexError;
 pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
     let transaction_bytes = hex::decode(raw_tx_hex);
     match transaction_bytes {
@@ -10,15 +9,6 @@ pub fn extract_tx_version(raw_tx_hex: &str) -> Result<u32, String> {
             let version_bytes = [bytes[0], bytes[1], bytes[2], bytes[3]];
             Ok(u32::from_le_bytes(version_bytes))
         }
-        Err(e) => {
-            if let FromHexError::InvalidHexCharacter { c, index } = e {
-                Err(format!(
-                    "Hex decode error: Invalid hex character '{}' at position {}",
-                    c, index
-                ))
-            } else {
-                Err(e.to_string())
-            }
-        }
+        Err(e) => Err(format!("Hex decode error: {}", e.to_string())),
     }
 }
